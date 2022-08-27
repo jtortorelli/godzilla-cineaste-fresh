301a7e38c50244e83880d21529f4c894e6cc717f
James Tortorelli <jtortorelli03@icloud.com>
2022-08-27 13:20:13 -0500

    edit home page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,13 @@
 /** @jsx h */
-import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import { h } from "preact";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
+        Welcome to The Godzilla Cineaste.
       </p>
-      <Counter start={3} />
     </div>
   );
 }
