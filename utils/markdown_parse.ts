0fd345a8cf2213380d34373e26149cd8683f71e6
James Tortorelli <jtortorelli03@icloud.com>
2022-09-13 21:09:36 -0500

    oops, forgot this

--- utils/markdown_parse.ts
+++ utils/markdown_parse.ts
@@ -2,9 +2,17 @@ import { render } from "gfm";
 import { extract } from "frontmatter";
 
 export default async function parseMarkdown(filename: string) {
-  const fileContents = await Deno.readTextFile(filename);
-  const { attrs, body } = extract(fileContents);
-  const renderedBody = render(body);
+  try {
+    const fileContents = await Deno.readTextFile(filename);
+    const { attrs, body } = extract(fileContents);
+    const renderedBody = render(body);
 
-  return { attrs, renderedBody };
+    return { attrs, renderedBody };
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      return { attrs: {}, renderedBody: "" };
+    } else {
+      throw error;
+    }
+  }
 }
